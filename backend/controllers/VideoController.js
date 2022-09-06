d90fa81b5bb41beeca4a07f609b82b6eb289c479
Samie <samrasolomon40@gmail.com>
2022-09-06 21:25:46 +0000

    Made a json response

--- backend/controllers/VideoController.js
+++ backend/controllers/VideoController.js
@@ -88,7 +88,7 @@ exports.createVideo = async (request, response) => {
         response.status(300).send('Video Exists');
       }
     } else {
-      response.status(404).send('Video URL is incorrect');
+      response.status(404).send({ message: 'Video URL is incorrect' });
     }
   }
 };
