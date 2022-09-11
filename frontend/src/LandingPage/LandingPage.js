0babf5ff4c5149fdfbcfb41f6810bd4c1e525359
Tonycrz <tonybaidoo0@gmail.com>
2022-09-11 22:57:48 +0000

    landing page updates

--- frontend/src/LandingPage/LandingPage.js
+++ frontend/src/LandingPage/LandingPage.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import { ImGithub, ImLinkedin } from 'react-icons/im';
 import { Login } from '../Login/Login';
 import { Signup } from '../Signup/Signup';
-import './LandingPage.css'
-import bgduck from './bgduck.jpg'
+import './LandingPage.css';
 
 
 export default class LandingPage extends React.Component {
@@ -25,7 +25,32 @@ export default class LandingPage extends React.Component {
     return(
       <>
       <div className="about-app">
-        {/* <img src={bgduck} width="500" height="400"></img> */}
+        <h1>Producktiv.</h1>
+        <div className="app-summary">
+          <p>
+          In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual 
+          form of a document or a typeface without relying on meaningful content. 
+          Lorem ipsum may be used as a placeholder before final copy is available.
+          </p>
+          <p>
+          In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual 
+          form of a document or a typeface without relying on meaningful content. 
+          Lorem ipsum may be used as a placeholder before final copy is available.
+          </p>
+        </div>
+        <div className="team-info">
+          <p>Built by:</p>
+          <ul>
+            <li><p>Opeyemi Ogunbode (Backend and DevOps)&nbsp;&nbsp;<a href="https://www.linkedin.com/in/opeyemi-ogunbode-33441814a/" target="_blank"><ImLinkedin/></a>&nbsp;&nbsp;<a href="https://github.com/Timi-T" target="_blank"><ImGithub/></a></p></li>
+            <li><p>Rihana (Backend)&nbsp;&nbsp;<a href="#0" target="_blank"><ImLinkedin/></a>&nbsp;&nbsp;<a href="https://github.com/avocadoclouds" target="_blank"><ImGithub/></a> </p></li>
+            <li><p> Samra Solomon (Backend)&nbsp;&nbsp;<a href="https://www.linkedin.com/in/samra-solomon-24b745208/" target="_blank"><ImLinkedin/></a>&nbsp;&nbsp;<a href="https://github.com/samie-ya" target="_blank"><ImGithub/></a></p></li>
+            <li><p>Tony Baidoo (Frontend)&nbsp;&nbsp;<a href="https://www.linkedin.com/in/tony-baidoo/" target="_blank"><ImLinkedin/></a>&nbsp;&nbsp;<a href="https://github.com/tcrz" target="_blank"><ImGithub/></a></p></li>
+          </ul>
+        </div>
+        <div>
+          <p><ImGithub/><a href="https://github.com/Timi-T/producktiv/">&nbsp;Link to Github Repo</a></p>
+        </div>
+        
       </div>
       <div className="login-section">
         <div className="login-header">
