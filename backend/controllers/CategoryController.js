6ca1490d9416645c42f8e929900ac1d6f093457f
Samie <samrasolomon40@gmail.com>
2022-09-11 15:53:53 +0000

    Removed redis key for categories and videos

--- backend/controllers/CategoryController.js
+++ backend/controllers/CategoryController.js
@@ -10,19 +10,12 @@ exports.getCategoryVideos = async (request, response) => {
     response.status(401).send({ message: 'Cookie Expired' });
   } else {
     const { name } = request.params;
-    const values = await redisClient.get(name);
-    if (!values) {
-      const category = await dbClient.get('categories', { name });
-      if (category) {
-        const lists = category.videos;
-        await redisClient.setCategory(name, JSON.stringify(lists));
-        response.status(200).send({ videos: category.videos });
-      } else {
-        response.status(404).send({ error: 'Category Doesn\'t exist' });
-      }
+    const category = await dbClient.get('categories', { name });
+    if (category) {
+      const lists = category.videos;
+      response.status(200).send({ videos: category.videos });
     } else {
-      const videos = JSON.parse(values);
-      response.status(200).send({ videos });
+      response.status(404).send({ error: 'Category Doesn\'t exist' });
     }
   }
 };
